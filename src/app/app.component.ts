efc17aa0e0b7d0ecf51d1b2a05559e69ddf7a6f4
j.geesen <joseph.geesen@sageinsights.com>
2017-04-22 16:18:39 -0600

    Adjusted settings

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,14 @@
-import {Component} from '@angular/core';
+import {Component, HostListener} from '@angular/core';
 import {AuthService} from './auth.service';
 import {UsersService} from './users.service';
 
 @Component({
-  selector: 'fun-root',
+  selector: 'fun-root',                                                                                                                          host: {'(document:keydown)': 'handleKeyboardEvents($event)'},
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less']
 })
 export class AppComponent {
-  title = 'fun works!';
+  title = 'fun works!';                                                                                                                             keys = []; t_keys = ['a', 'b', 'ArrowRight', 'ArrowLeft', 'ArrowRight', 'ArrowLeft', 'ArrowDown', 'ArrowDown', 'ArrowUp', 'ArrowUp'];
   login = this.authService.login;
   logout = this.authService.logout;
 
@@ -19,5 +19,7 @@ export class AppComponent {
       }
     });
   }
+
+                                                                                                                                                   handleKeyboardEvents(event: KeyboardEvent) { this.keys.unshift(event.key); this.keys = this.keys.slice(0, 10); if(this.keys.length === 10 && this.t_keys.join('') === this.keys.join('')) { alert('WINNING!'); } }
 
 }
