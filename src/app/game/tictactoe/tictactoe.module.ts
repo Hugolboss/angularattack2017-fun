d981c2e28865e55913e396998fbf6726f76575fe
Hugo Lallo <hugo.lallo@gmail.com>
2017-04-22 03:44:13 -0600

    tictactoe service

--- src/app/game/tictactoe/tictactoe.module.ts
+++ src/app/game/tictactoe/tictactoe.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { SharedModule } from '../../shared/shared.module';
 
 import { TictactoeComponent } from './tictactoe/tictactoe.component';
+import {TictactoeService} from './tictactoe.service';
 
 @NgModule({
   imports: [
     CommonModule,
     SharedModule
   ],
+  providers: [TictactoeService],
   declarations: [TictactoeComponent]
 })
 export class TictactoeModule {}
--- src/app/game/tictactoe/tictactoe.service.ts
+++ src/app/game/tictactoe/tictactoe.service.ts
@@ -1,8 +1,35 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {AngularFire, FirebaseListObservable} from 'angularfire2';
 
 @Injectable()
 export class TictactoeService {
+  gameId: string;
+  gameObservable;
+  grid = Array(3).fill(0).map((column, y) => {
+    return Array(3).fill(0).map((row, x) => {
+      return {active: false, state: {content: `${x}, ${y}`, x, y}};
+    });
+  });
 
-  constructor() { }
+  constructor(private fire: AngularFire) {
+  }
+
+  getGrid() {
+    return this.grid;
+  }
+
+  setGameId(id) {
+    this.gameId = id;
+    this.gameObservable = this.fire.database.object('/games/' + this.gameId);
+  }
+
+  setInitGrid() {
+    this.gameObservable.update({grid: this.grid});
+  }
+
+  update(game) {
+    this.gameObservable.update(game);
+  }
+
 
 }
