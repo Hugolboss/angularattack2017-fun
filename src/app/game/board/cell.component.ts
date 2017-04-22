cf925d69979bed97430165dcb22d3b8e1b91ba6f
ron.salmon <ron.salmon@sageinsights.com>
2017-04-21 18:56:31 -0600

    base cell structure

---
+++ src/app/game/board/cell.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'cell',
+  templateUrl: './cell.component.html',
+  styleUrls: ['./cell.component.less']
+})
+export class CellComponent {
+  active: boolean;
+  state: Object;
+
+  constructor(active: boolean, state: Object) {
+    this.active = active;
+    this.state = state;
+  }
+}
