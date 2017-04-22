7d62e48d8af241db470ab4185738e1c4d21289dd
Hugo Lallo <hugo.lallo@gmail.com>
2017-04-21 23:37:59 -0600

    tick tac toe content

--- src/app/game/tictactoe/tictactoe/tictactoe.component.ts
+++ src/app/game/tictactoe/tictactoe/tictactoe.component.ts
@@ -12,35 +12,53 @@ export class TictactoeComponent implements OnInit {
   grid;
   symbols = ['x', 'o'];
   players = [
-    {id: 123435, name: 'player1', icon: ''},
-    {id: 678998, name: 'player2', icon: ''}
+    {id: 123435, name: 'player1', icon: '', ind: -1},
+    {id: 678998, name: 'player2', icon: '', ind: -1}
   ];
-  lastPlayer;
+  currentPlayer;
   constructor(private route: ActivatedRoute) {}
 
   ngOnInit() {
-    this.players = this.players.map((player, i) => { player.icon = this.symbols[i];
+    this.players = this.players.map((player, i) => {
+      player.ind = i;
+      player.icon = this.symbols[i];
       return player;
     });
-    this.grid = Array(Array(3), Array(3), Array(3)).map(row => {
-      return row.fill({active: false, state: {content: '' }});
-    });
-    console.log(this.players, this.grid);
+    if (!this.currentPlayer) {
+      this.currentPlayer = this.players[0];
+    }
+    this.grid = [
+      [
+        {active: true, state: {content: '0, 0', x: 0, y: 0}},
+        {active: false, state: {content: '0, 1', x: 1, y: 0}},
+        {active: false, state: {content: '0, 2', x: 2, y: 0}},
+      ],
+      [
+        {active: true, state: {content: '1, 0', x: 0, y: 1}},
+        {active: false, state: {content: '1, 1', x: 1, y: 1}},
+        {active: false, state: {content: '1, 2', x: 2, y: 1}},
+      ],
+      [
+        {active: true, state: {content: '2, 0', x: 0, y: 2}},
+        {active: false, state: {content: '2, 1', x: 1, y: 2}},
+        {active: false, state: {content: '2, 2', x: 2, y: 2}},
+      ]
+    ];
     this.route.params
       .subscribe((params: Params) => { this.gameId = params['id']; });
   }
 
-  switchPlayer() {
-    if (this.currentContent === 'X') {
-      this.currentContent = 'O';
-    } else {
-      this.currentContent = 'X';
+  switchPlayer(idx) {
+    if (idx === 0) {
+      this.currentPlayer = this.players[1];
+    }else {
+      this.currentPlayer = this.players[0];
     }
   }
 
   onClick(state) {
-    this.grid[state.y][state.x].state.content = this.currentContent;
-    this.switchPlayer();
+    this.grid[state.y][state.x].state.content = this.currentPlayer.icon;
+    this.switchPlayer(this.currentPlayer.ind);
   }
 
 }
