27d641fd9d9ea3e28044bf3bf3d5f0ba64b986d0
Hugo Lallo <hugo.lallo@gmail.com>
2017-04-21 19:10:30 -0600

    TickTackToeModule && cell rename

--- src/app/game/board/cell.component.ts
+++ src/app/game/board/cell.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'cell',
+  selector: 'fun-cell',
   templateUrl: './cell.component.html',
   styleUrls: ['./cell.component.less']
 })
--- src/app/game/tictactoe/tictactoe.module.ts
+++ src/app/game/tictactoe/tictactoe.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {SharedModule} from '../../shared/shared.module';
+import {CellComponent} from '../board/cell.component';
 
 @NgModule({
   imports: [
     CommonModule,
-    SharedModule
+    SharedModule,
+    CellComponent
   ],
   declarations: []
 })
