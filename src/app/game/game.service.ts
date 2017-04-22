6b9cd9387fc18368904bac6b6ac4349766332541
mrhedges <michael.hedges@dn2k.com>
2017-04-22 04:45:19 -0600

    undo

--- src/app/game/game.service.ts
+++ src/app/game/game.service.ts
@@ -8,50 +8,18 @@ export class GameService {
   constructor() { }
 
   newGame = (game, player) => {
-
-
-
-
     return firebase.database().ref('games/').push({
-
-
-
-
       'game': game,
-
-
-
-
-
       'players': [ {displayName: player.displayName, id: player.uid, ind: 0} ]
     }).key;
   }
-
   joinGame = (key, player) => {
-
-
-    
     firebase.database().ref('games/' + key)  
-
-
-
-
-
         .once('value')
         .then(snapshot => [snapshot.val().players[0], {displayName: player.displayName, id: player.uid, ind: 1}])
         .then(players => {
-
-
-
-
-
           firebase.database().ref('games/' + key).update({'players': players});
         })
-
-
-
-
-
         .catch(err => console.log('something happened:', err));
   }
 }
