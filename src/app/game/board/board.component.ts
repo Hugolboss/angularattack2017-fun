68b1bc2e79d7da8c31f0c0cf5e3899edb9460df0
ron.salmon <ron.salmon@sageinsights.com>
2017-04-23 05:21:45 -0600

    feat: switch to mouseleave to reduce number of events

--- src/app/game/board/board.component.ts
+++ src/app/game/board/board.component.ts
@@ -9,7 +9,7 @@ import _ from 'lodash';
 export class BoardComponent implements OnInit{
   @Output('onClick') click: EventEmitter<any> = new EventEmitter();
   @Output('onMouseOver') mouseOver: EventEmitter<any> = new EventEmitter();
-  @Output('onMouseOut') mouseOut: EventEmitter<any> = new EventEmitter();
+  @Output('onMouseLeave') mouseLeave: EventEmitter<any> = new EventEmitter();
   @Input() grid: Object[][];
   @Input() x: number;
   @Input() y: number;
@@ -32,7 +32,7 @@ export class BoardComponent implements OnInit{
     this.mouseOver.emit(cell);
   }
 
-  onMouseOut(cell) {
-    this.mouseOut.emit(cell);
+  onMouseLeave(cell) {
+    this.mouseLeave.emit(cell);
   }
 }
