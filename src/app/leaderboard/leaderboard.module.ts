863654fb47faeb0a46d38b1ab9583ca53bc9f1d0
mrhedges <michael.hedges@dn2k.com>
2017-04-23 15:19:44 -0600

    leaderboard module

--- src/app/leaderboard/leaderboard.module.ts
+++ src/app/leaderboard/leaderboard.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SharedModule } from './../shared/shared.module';
 import { LeaderboardComponent } from './leaderboard.component';
+import { MaterialModule } from './../shared/material/material.module';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    MaterialModule,
+    SharedModule
   ],
   declarations: [LeaderboardComponent]
 })
