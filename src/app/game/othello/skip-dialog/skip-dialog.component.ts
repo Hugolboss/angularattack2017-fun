46675c00879ccb7995ff0573e1e50519684f1e9b
ron.salmon <ron.salmon@sageinsights.com>
2017-04-23 05:22:48 -0600

    feat: skip dialog

---
+++ src/app/game/othello/skip-dialog/skip-dialog.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'fun-skip-dialog',
+  templateUrl: './skip-dialog.component.html',
+  styleUrls: ['./skip-dialog.component.less']
+})
+export class SkipDialogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
