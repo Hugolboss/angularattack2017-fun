fb5a60782c53948a4a0807bde7e8d38ab4b173b3
mrhedges <michael.hedges@dn2k.com>
2017-04-23 15:12:17 -0600

    leaderboard component

--- src/app/leaderboard/leaderboard.component.ts
+++ src/app/leaderboard/leaderboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UsersService } from './../users.service';
 
 @Component({
   selector: 'fun-leaderboard',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LeaderboardComponent implements OnInit {
 
-  constructor() { }
+  constructor(private usersService: UsersService) { }
 
   ngOnInit() {
   }
